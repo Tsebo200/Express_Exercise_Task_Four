a132474a4565fbf42cff15351082d2daf3d64755
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-08-05 12:51:30 +0200

    Working on addProduct CRUD

--- index.js
+++ index.js
@@ -8,30 +8,40 @@ app.get('/', (req, res) => {
 })
 
 const refine = (req,res,next) => {
-    let value = products.inventory[0];
+    let value = products.inventory[0].productName;
     res.send(value)
     next();
 };
+
+app.use(refine); //This function is reserved for middleware
 
 //All products
 app.get('/api/products/:id', (req, res) => {
     res.send(products);
 });
 
-//Individual
-// app.get('/api/products/:id', (req, res) => {
+//Individual Products
+app.get('/api/products/:id', (req, res) => {
 
-//     const found = products.inventory.some(item => item.id == parseInt(req.params.id));
+    const found = products.inventory.some(item => item.id == parseInt(req.params.id));
 
-//     if(found === true){
-//         res.json(products.inventory.filter(item => item.id === parseInt(req.params.id)));
-//     } else {
-//         res.status(400).json({msg: "404Err :/ Sorry Mate This product was not found"});
-//     }
+    if(found === true){
+        res.json(products.inventory.filter(item => item.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: "404Err :/ Sorry Mate This product was not found"});
+    }
 
-//     res.json(products.inventory.filter(item => item.id === parseInt(req.params.id)));
-// });
+    res.json(products.inventory.filter(item => item.id === parseInt(req.params.id)));
+});
 
+app.post('/api/addProduct/:id', (req,res) => {
+    const newProduct = {
+        id: req.params.id,
+        productName: req.body.productName,
+        inStock: req.body.inStock,
+        availStock: req.body.availStock
+    }
+});
 
 const PORT = process.env.PORT || 5002;
 
