951c0247586b054d71613c1c2a7725e0f67cb556
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-08-05 12:42:55 +0200

    Started Middleware
    Completed the integration of previous file and moved to PORT 5002

--- index.js
+++ index.js
@@ -4,20 +4,35 @@ const products = require('./Products');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Ohiyo");
+    res.send("Ohiyo Onisan");
 })
 
-//All products
-// app.get('/api/products/:id', (req, res) => {
-//     res.send(products);
-// });
+const refine = (req,res,next) => {
+    let value = products.inventory[0];
+    res.send(value)
+    next();
+};
 
-//Individual
+//All products
 app.get('/api/products/:id', (req, res) => {
-    res.json(products.inventory.filter(item => item.id === parseInt(req.params.id)));
+    res.send(products);
 });
 
+//Individual
+// app.get('/api/products/:id', (req, res) => {
 
-const PORT = process.env.PORT || 5001;
+//     const found = products.inventory.some(item => item.id == parseInt(req.params.id));
+
+//     if(found === true){
+//         res.json(products.inventory.filter(item => item.id === parseInt(req.params.id)));
+//     } else {
+//         res.status(400).json({msg: "404Err :/ Sorry Mate This product was not found"});
+//     }
+
+//     res.json(products.inventory.filter(item => item.id === parseInt(req.params.id)));
+// });
+
+
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {console.log(`Server Started on port ${PORT}`)});
